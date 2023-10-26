c12f2b7cf588f84f8566fa05f06fec187137df8f
IrziAja <tmr.irzi1533@gmail.com>
2023-10-26 15:39:38 +0700

    Update

--- Main.py
+++ Main.py
@@ -1,6 +1,5 @@
 from math_function import add
 
-
 def main():
 
     data_1 = int(input("masukkan input 1 :"))
@@ -9,10 +8,21 @@ def main():
 
     if operator == "+":
         result = add(data_1, data_2)
+    elif operator == "*":  # Check for multiplication
+        result = multiply(data_1, data_2)
+    elif operator == "/":  # Check for division
+        try:
+            result = divide(data_1, data_2)
+        except ValueError as e:
+            print("Error:", e)
+            return  # Exit the program in case of division by zero
+    else:
+        print("Operator not supported")
+        return  # Exit the program for unsupported operators
 
     print("{} {} {} = {} ".format(data_1, operator, data_2, result))
 
 
-if __name__ == "__main__":
+if _name_ == "_main_":
     print("Hello Main !")
     main()
